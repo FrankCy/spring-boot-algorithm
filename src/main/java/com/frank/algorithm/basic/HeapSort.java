06cf80cde8a7be9d4d0bbc47767d1eb098d83ead
cyoung <cy880708@163.com>
2019-04-30 16:46:22 +0800

    提交堆排序和归并排序的实现

--- src/main/java/com/frank/algorithm/basic/HeapSort.java
+++ src/main/java/com/frank/algorithm/basic/HeapSort.java
@@ -11,4 +11,72 @@ package com.frank.algorithm.basic;
  * @mofified By:
  */
 public class HeapSort {
+
+    private int[] arr;
+
+    public void heapAdjust(int[] arr,int s,int m){
+        // 已知arr[s...m]中记录的关键字除arr[s]之外均满足堆的定义，本函数调整arr[s]的关键字，使arr[s...m]成为一个最大堆
+        // s是最后一个非叶子节点
+        int rc = arr[s];
+
+        for(int j=2*s;j <= m;j = s*2){
+            if(j<m && arr[j]<arr[j+1]) {
+                // j为key较大的下标
+                j++;
+            }
+            if(rc >= arr[j]) {
+                break;
+            }
+            // 上移到父节点
+            arr[s] = arr[j];
+            s=j;
+        }
+        // 要放入的位置
+        arr[s]=rc;
+    }
+
+    public void outPutArr(int i) {
+
+        if(i <= arr[0]){
+            System.out.println("outPutArr: " + arr[i]);
+            // 左
+            outPutArr(i*2);
+            // 右
+            outPutArr(i*2+1);
+        }
+
+    }
+
+    public void heapSort(int[] arr) {
+
+        // 对数组进行建堆操作，就是从最后一个非叶结点进行筛选的过程
+        // 因为0没有使用，所以length-1
+        for(int i=(arr.length-1)/2; i > 0; i--) {
+            heapAdjust(arr,i,arr.length-1);
+        }
+
+        System.out.println("........建堆完成.............");
+
+        outPutArr(1);
+        for(int i=arr.length-1; i>1; i--){
+            int temp = arr[1];
+            arr[1] = arr[i];
+            arr[i] = temp;
+            heapAdjust(arr,1,i-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        HeapSort heapSor = new HeapSort();
+        // 0下标放的是数组长度
+        int[] arr = {7,23,45,9,40,73,12,49};
+        heapSor.arr = arr;
+        heapSor.heapSort(arr);
+
+        for(int i :arr) {
+            System.out.print(".." + i);
+        }
+        System.out.println("\n" +".......排序结束.......");
+    }
 }
--- src/main/java/com/frank/algorithm/basic/MergeSort.java
+++ src/main/java/com/frank/algorithm/basic/MergeSort.java
@@ -1,5 +1,7 @@
 package com.frank.algorithm.basic;
 
+import java.util.Random;
+
 /**
  * @version 1.0
  * @description：归并排序
@@ -11,4 +13,74 @@ package com.frank.algorithm.basic;
  * @mofified By:
  */
 public class MergeSort {
+
+    /**
+     * @description：归并排序
+     * @version 1.0
+     * @author: Yang.Chang
+     * @email: cy880708@163.com
+     * @date: 2019/4/30 下午4:07
+     * @mofified By:
+     */
+    public static void mergeSort(int[] a, int low, int mid, int high) {
+        int[] temp = new int[high-low+1];
+        int i= low;
+        int j = mid+1;
+        int k=0;
+        // 把较小的数先移到新数组中
+        while(i<=mid && j<=high){
+            if(a[i]<a[j]){
+                temp[k++] = a[i++];
+            }else{
+                temp[k++] = a[j++];
+            }
+        }
+        // 把左边剩余的数移入数组
+        while(i<=mid){
+            temp[k++] = a[i++];
+        }
+        // 把右边边剩余的数移入数组
+        while(j<=high){
+            temp[k++] = a[j++];
+        }
+        // 把新数组中的数覆盖nums数组
+        for(int x=0;x<temp.length;x++){
+            a[x+low] = temp[x];
+        }
+    }
+
+    /**
+     * @description：排序
+     * @version 1.0
+     * @author: Yang.Chang
+     * @email: cy880708@163.com
+     * @date: 2019/4/30 下午4:10
+     * @mofified By:
+     */
+    public static int[] sort(int[] a,int low,int high){
+        // 分解
+        int mid = (low+high)/2;
+        if(low<high){
+            // 治理
+            sort(a,low,mid);
+            // 治理
+            sort(a,mid+1,high);
+            // 左右归并（合并）
+            mergeSort(a,low,mid,high);
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[10];
+        for(int i=0; i<10; i++) {
+            a[i] = new Random().nextInt();
+        }
+
+        int[] b = sort(a, 0,a.length - 1);
+        for(int y : b) {
+            System.out.println(y);
+        }
+    }
+
 }
